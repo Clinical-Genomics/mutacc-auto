22bf2c16411a22434df4b5864d3d88fdadc1a41c
Adam Rosenbaum <adam.rosenbaum@scilifelab.se>
2019-08-12 09:49:51 +0200

    fix

--- mutacc_auto/cli/export_command.py
+++ mutacc_auto/cli/export_command.py
@@ -48,7 +48,7 @@ def export_command(ctx, vcf_out, background, dataset_dir, conda, environment, dr
 
     mutacc_config = ctx.obj['mutacc_config']
     mutacc_binary = ctx.obj.get('mutacc_binary')
-    mutacc_environment = ctx.obj.get('mutacc_environment') or environment
+    mutacc_environment = environment or ctx.obj.get('mutacc_environment')
 
     slurm_options = ctx.obj['slurm_options']
 
